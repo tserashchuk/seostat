8a4403623272c2b8c3f7e25dcd897a1afceafffc
tserashchuk <tserashchuk.kirill@gmail.com>
2025-03-13 19:17:07 +0300

    fdv

--- seostat/celery.py
+++ seostat/celery.py
@@ -19,12 +19,16 @@ app =  Celery('seostat',
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
+    logger.info("Hello")
     print(f'Request: {self.request!r}')
